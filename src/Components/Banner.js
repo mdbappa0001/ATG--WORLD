e111f5d80c4f62456545959f36cb49c56221cfb0
mdbappa0001 <abutanif366@gmail.com>
2022-07-29 02:12:03 +0600

    banner added

---
+++ src/Components/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import banner from "../images/banner.png";
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <>
+            <section id='banner' style={{
+                background: `url(${banner})`,
+                paddingTop: "286px",
+                paddingBottom: "80px",
+                backgroundBlendMode: "overlay",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: "center",
+                backgroundSize: "cover"
+            }}>
+                <div className="banner-container">
+                    <div className='banner_text'>
+                        <h3>Computer Engineering</h3>
+                        <p>142,765 Computer Engineers follow this</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Banner;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
 import MenuBar from './MenuBar';
 
 const Home = () => {
     return (
         <div>
             <MenuBar></MenuBar>
+            <Banner></Banner>
         </div>
     );
 };
